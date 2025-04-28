e031fae139f01bbf09e35f9a85282e91837322cb
Habib Ali Machpud <habib.machpud@gmail.com>
2025-04-28 22:58:09 +0700

    init

---
+++ src/main/java/io/github/bluething/playground/java/App.java
@@ -0,0 +1,13 @@
+package io.github.bluething.playground.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
