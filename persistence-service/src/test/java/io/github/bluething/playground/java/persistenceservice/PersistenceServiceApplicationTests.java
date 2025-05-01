559ee39bd80e64f7fafac11a8b3bb183fd301959
Habib Ali Machpud <habib.machpud@gmail.com>
2025-05-01 16:45:55 +0700

    Add new module

---
+++ persistence-service/src/test/java/io/github/bluething/playground/java/persistenceservice/PersistenceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bluething.playground.java.persistenceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersistenceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
